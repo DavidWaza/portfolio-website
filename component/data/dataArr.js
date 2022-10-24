7928f086eea7760de8b286628bfbd067ba80d4c5
DavidWaza <wazabanga00@gmail.com>
2022-10-24 21:10:26 +0100

    initial commit

---
+++ component/data/dataArr.js
@@ -0,0 +1,27 @@
+import LinkedIn from "../../public/linkedin.png";
+import Github from "../../public/github.png";
+import Twitter from "../../public/twitter.png";
+
+export const SocialData = [
+  {
+    id: 1,
+    img: LinkedIn,
+    Status: "LinkedIn",
+    followers: "106 connections",
+    url:"www.linkedin.com/in/idemeto-emediong"
+  },
+  {
+    id: 2,
+    img: Twitter,
+    Status: "Twitter",
+    followers: "70 followers",
+    url: "https://twitter.com/WazaBanga01"
+  },
+  {
+    id: 3,
+    img: Github,
+    Status: "Github",
+    followers: "1 follower",
+    url: "https://github.com/DavidWaza/"
+  },
+];
